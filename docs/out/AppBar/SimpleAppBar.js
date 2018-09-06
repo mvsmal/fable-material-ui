ef580f7fe84fac07088eb4c8adb1b8c18bbb1ad0
Mikhail Smal <mvsmal@gmail.com>
2018-09-06 23:50:32 +0200

    Deleted out folder

--- docs/out/AppBar/SimpleAppBar.js
+++
@@ -1,6 +0,0 @@
-import { createElement } from "react";
-import { Typography, Toolbar, AppBar, materialEl } from "../src/Fable.Helpers.MaterialUI";
-import { ofArray } from "../fable-core/List";
-import List from "../fable-core/List";
-import { Props } from "../src/Fable.Helpers.MaterialUI.Props";
-export const view = createElement("div", {}, materialEl(AppBar, ofArray([new Props.AppBarProp(0, "static"), new Props.MaterialProp(4, "default")]), ofArray([materialEl(Toolbar, new List(), ofArray([materialEl(Typography, ofArray([new Props.TypographyProp(6, "title"), new Props.MaterialProp(4, "inherit")]), ofArray(["Photos"]))]))])));

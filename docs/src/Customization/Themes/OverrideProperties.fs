a7999c2347f5546814594ae7753c772cfd42d3db
Mikhail Smal <mvsmal@gmail.com>
2018-10-25 22:22:36 +0200

    Added Override properties docs

---
+++ docs/src/Customization/Themes/OverrideProperties.fs
@@ -0,0 +1,21 @@
+module Customization.Themes.OverrideProperties
+
+open Fable.Helpers.React
+open Fable.Helpers.MaterialUI
+open Fable.MaterialUI.Props
+
+let theme = createMuiTheme [
+                ThemeProp.Props [
+                    // Name of the component ⚛️
+                    ThemePropsProp.MuiButtonBase [
+                        // The properties to apply
+                        MaterialProp.DisableRipple true // No more ripple, on the whole application 💣!
+                    ]
+                ]
+            ]
+
+let view () =
+    muiThemeProvider [ MuiThemeProviderProp.Theme (ProviderTheme.Theme theme) ] [
+        button [] [ str "Override properties" ]
+    ]
+
